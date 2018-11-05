2be478d36adff177ca91393bf5ca8180cc94a093
luffyerrot <44428830+luffyerrot@users.noreply.github.com>
2018-11-05 17:47:08 +0100

    Delete mainTestString.java

--- jeu logique/src/com/pierre/ocr/mainTestString.java
+++
@@ -1,9 +0,0 @@
-package com.pierre.ocr;
-
-public class mainTestString {
-
-    public static void main(String[] args) {
-        CorpTestString corpTestString = new CorpTestString();
-        corpTestString.jeu();
-    }
-}
